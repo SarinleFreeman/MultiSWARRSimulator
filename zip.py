a0e8a1e3f50eda8e4f87dbd78f89dba47782bb55
SarinleFreeman <sarinlefreeman@gmail.com>
2023-04-05 11:04:24 +0800

    house cleaning

--- zip.py
+++
@@ -1,9 +0,0 @@
-import shutil
-
-# specify the folder path
-folder_to_zip = 'simulation_data'
-
-# create a Zip file of the folder and its subfolders recursively
-shutil.make_archive('simulation_data', 'zip', folder_to_zip)
-
-print('Folder zipped successfully!')
